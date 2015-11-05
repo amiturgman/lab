5fbf380178a5088252e5ad516307098fd8f44370
amiturgman <ami.turgman@microsoft.com>
2015-11-05 13:38:47 +0200

    adding time

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var port = process.env.PORT || 3001;
 
 http.createServer(function(req,res){
-	res.end('hello world');
+	res.end('hello world: ' + new Date().getTime());
 }).listen(port, function(){
 	console.log('listening on port', port);
 });
